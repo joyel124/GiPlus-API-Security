bbd96d0090b0360f1b3ef9af3fc967e7086829c0
joyel <geolrco124@hotmail.com>
2022-11-19 20:09:50 -0500

    feat: Changed Environment to IsProduction.

--- GiPlus.API/Program.cs
+++ GiPlus.API/Program.cs
@@ -107,7 +107,7 @@ using (var context = scope.ServiceProvider.GetService<AppDbContext>())
 }
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI(options =>
